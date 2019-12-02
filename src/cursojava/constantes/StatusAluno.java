ed6408ae393f6c8b5f816164480771f91c0397df
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2019-12-01 22:21:20 -0300

    Inserindo Login e Senha para validação e permissão do uso do sistema

---
+++ src/cursojava/constantes/StatusAluno.java
@@ -0,0 +1,9 @@
+package cursojava.constantes;
+
+public class StatusAluno {
+	
+	/*Classe e constante estáticas*/	
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERACAO = "Recuperação";
+	public static String REPROVADO = "Reprovado";
+}
